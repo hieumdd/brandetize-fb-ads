4ec8a179f9a5a009b26b017ce7d4cb18f9ae2d03
HM <hieumdd@gmail.com>
2024-02-13 11:07:06 +0700

    feat: Add createExternalTable

--- src/bigquery.service.ts
+++ src/bigquery.service.ts
@@ -2,6 +2,8 @@ import { BigQuery } from '@google-cloud/bigquery';
 
 import { getLogger } from './logging.service';
 
+const logger = getLogger(__filename);
+
 const client = new BigQuery();
 
 const DATASET = 'Facebook';
@@ -24,4 +26,32 @@ export const createLoadStream = (options: CreateLoadStreamConfig, table: string)
         .on('job', () => {
             getLogger(__filename).debug({ dataset: DATASET, table, schema: options.schema });
         });
+};
+
+type CreateExternalTableOptions = {
+    name: string;
+    sourceUris: string[];
+    schema: any[];
+    sourceUriPrefix?: string;
+};
+
+export const createExternalTable = async (dataset: string, options: CreateExternalTableOptions) => {
+    const { name: tableName, sourceUris, schema, sourceUriPrefix } = options;
+
+    const table = client.dataset(dataset).table(tableName);
+    if (await table.exists().then(([response]) => response)) {
+        logger.debug(`table ${table.id} exists, replacing`);
+        await table.delete();
+    }
+    await table.create({
+        schema,
+        externalDataConfiguration: {
+            sourceUris,
+            sourceFormat: 'NEWLINE_DELIMITED_JSON',
+            ignoreUnknownValues: true,
+            hivePartitioningOptions: sourceUriPrefix
+                ? { mode: 'CUSTOM', sourceUriPrefix }
+                : undefined,
+        },
+    });
 };
