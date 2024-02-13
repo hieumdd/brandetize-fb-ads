3bfd36318b0889a97b770e276e23f2f9bd42f95b
HM <hieumdd@gmail.com>
2024-02-13 22:08:47 +0700

    misc: Remove createLoadStream

--- src/google-cloud/bigquery.service.ts
+++ src/google-cloud/bigquery.service.ts
@@ -4,29 +4,7 @@ import { getLogger } from '../logging.service';
 
 const logger = getLogger(__filename);
 
-const client = new BigQuery();
-
-const DATASET = 'Facebook';
-
-export type CreateLoadStreamConfig = {
-    schema: Record<string, any>[];
-    writeDisposition: 'WRITE_APPEND' | 'WRITE_TRUNCATE';
-};
-
-export const createLoadStream = (options: CreateLoadStreamConfig, table: string) => {
-    return client
-        .dataset(DATASET)
-        .table(table)
-        .createWriteStream({
-            schema: { fields: options.schema },
-            sourceFormat: 'NEWLINE_DELIMITED_JSON',
-            createDisposition: 'CREATE_IF_NEEDED',
-            writeDisposition: options.writeDisposition,
-        })
-        .on('job', () => {
-            logger.debug({ dataset: DATASET, table, schema: options.schema });
-        });
-};
+const bigqueryClient = new BigQuery();
 
 type CreateExternalTableOptions = {
     name: string;
@@ -38,7 +16,7 @@ type CreateExternalTableOptions = {
 export const createExternalTable = async (dataset: string, options: CreateExternalTableOptions) => {
     const { name: tableName, sourceUris, schema, sourceUriPrefix } = options;
 
-    const table = client.dataset(dataset).table(tableName);
+    const table = bigqueryClient.dataset(dataset).table(tableName);
     if (await table.exists().then(([response]) => response)) {
         logger.debug(`Replacing table ${table.id}`);
         await table.delete();
