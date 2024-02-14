696d5b5fd23e1d42a1f37d9528df3f1f878df720
HM <hieumdd@gmail.com>
2024-02-14 23:38:44 +0700

    misc: Change test names

--- src/pipeline/pipeline.service.test.ts
+++ src/pipeline/pipeline.service.test.ts
@@ -1,9 +1,9 @@
 import * as pipelines from './pipeline.const';
 import { createInsightsPipelineTasks, runInsightsPipeline } from './pipeline.service';
 
-describe('pipeline', () => {
+describe('runInsightsPipeline', () => {
     it.each([pipelines.AdsInsights])(
-        '$name',
+        'runInsightsPipeline/$name',
         async (pipeline) => {
             return await runInsightsPipeline(pipeline, {
                 accountId: '662385836106323',
@@ -20,7 +20,7 @@ describe('pipeline', () => {
     );
 });
 
-it('create-tasks-insights', async () => {
+it('createInsightsPipelineTasks', async () => {
     return await createInsightsPipelineTasks({
         start: '2023-08-28',
         end: '2023-09-04',
